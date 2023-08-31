9751daab4ee5d74f600c6032e9d94495866f2d1a
canyucelb <93741400+canyucelb@users.noreply.github.com>
2023-08-31 17:04:05 -0400

    Add files via upload
     C++ program that prints your first and last name 
    
    inludes
    1. CMakeList.txt
    sourcecode(***.cpp)
    binary file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    std::cout << "First Name: Baran Can" << std::endl;
+    std::cout << "Last Name: Yucel" << std::endl;
+    return 0;
+}
+
+
+
+
